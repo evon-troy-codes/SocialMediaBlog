598ab8c7c03ed4a2b245bbcaaf59690fb64245c0
Evon Troy Alexander <evon.codes@gmail.com>
2026-01-08 19:19:51 -0500

    Remove instructional header comments from source files

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -11,11 +11,6 @@ import io.javalin.Javalin;
 import io.javalin.http.Context;
 import java.util.List;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
 public class SocialMediaController {
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
